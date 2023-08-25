154840f5cbfca6667a9f41f29a2373ea3aff1d8d
ang s <angsala@umich.edu>
2023-08-25 17:54:40 -0500

    Basic 3D top down player control
    - Implemented 2d movement and jumping
    - Player model turns with last direction of movement
    - Implemented top down camera following player model
    
    Next step- Autumn pull this basic player model to use for environment design. Next to work on is melee/enemy most likely after basic level environment.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent (typeof (CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed; //control speed of character
+    private float currentVelocity;
+    [SerializeField] private float smoothTime = .05f;
+    private Vector2 move; //store vector values from input
+    private Vector3 direction;
+ // private PlayerInputActions input;
+ private CharacterController characterController;
+private float gravity = -9.81f;
+[SerializeField] private float gravityMultiplier = 3.0f;
+private float velocity = -1.0f;
+[SerializeField] private float jumpForce;
+    public void OnMove(InputAction.CallbackContext context){
+        move = context.ReadValue<Vector2>();
+        direction = new Vector3(move.x, 0.0f, move.y);
+    }
+    // Start is called before the first frame update
+
+    private void Awake(){
+        characterController = GetComponent<CharacterController>();
+        
+    }
+
+    void Start(){
+        velocity = -1.0f;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplyGravity();
+        movePlayer();
+    }
+
+    public void movePlayer(){
+        
+        if (move.sqrMagnitude == 0) return;
+        var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        characterController.Move(direction *speed * Time.deltaTime);
+    }
+
+    public void onJump(InputAction.CallbackContext context){
+        if (!context.started){
+            Debug.Log("Not jumping");
+            return;
+        }
+        if (!IsGrounded()){
+            return;
+        }
+
+        velocity += jumpForce;
+
+    }
+
+    private void ApplyGravity(){
+        if (IsGrounded() && velocity < 0.0f){
+            velocity = -1.0f;
+        } 
+
+        else {
+            velocity += gravity *gravityMultiplier * Time.deltaTime;
+        }
+
+            direction.y = velocity;
+            characterController.Move(direction *speed * Time.deltaTime);
+        
+    }
+
+    private bool IsGrounded() => characterController.isGrounded;
+}

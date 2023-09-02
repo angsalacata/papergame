63d6d9b6b68b42cf5e653c3e43c87f776c65f4a2
angsalacata <angsala@umich.edu>
2023-09-02 09:24:45 -0400

    Modified view for better 2.5 added sample 2d assets and basic animation
    - Modified camera angle and sprite rotation to 45 deg for better 2.5 view. Collider is orthogonal to ground while sprite is 45 deg tilted toward camera
    - removed jumping and gravity. jumping not needed and gravity to be added again later
    - added free 2d assets from pixel adventures
    - implemented basic walking and idle animation for character movement. walking is only horizontal walking right now

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,8 +11,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float smoothTime = .05f;
     private Vector2 move; //store vector values from input
     private Vector3 direction;
- // private PlayerInputActions input;
+
  private CharacterController characterController;
+
+// boolean to set walking animation condition
+private static readonly int WALK_PROPERTY = Animator.StringToHash("Walking");
+
+
+
+ [Header("Relations")]
+    [SerializeField]
+    private Animator animator = null;
+
+    [SerializeField]
+    private SpriteRenderer spriteRenderer = null;
 private float gravity = -9.81f;
 [SerializeField] private float gravityMultiplier = 3.0f;
 private float velocity = -1.0f;
@@ -20,6 +32,17 @@ private float velocity = -1.0f;
     public void OnMove(InputAction.CallbackContext context){
         move = context.ReadValue<Vector2>();
         direction = new Vector3(move.x, 0.0f, move.y);
+
+        // moving right 
+        if (move.x > 0){
+            spriteRenderer.flipX = false;
+        }
+
+        // moving left
+        else if (move.x < 0){
+            spriteRenderer.flipX = true;
+        }
+
     }
     // Start is called before the first frame update
 
@@ -36,22 +59,25 @@ private float velocity = -1.0f;
     // Update is called once per frame
     void Update()
     {
-        ApplyGravity();
+        // ApplyGravity();
         movePlayer();
     }
 
     public void movePlayer(){
         
-        if (move.sqrMagnitude == 0) return;
+        if (move.sqrMagnitude == 0) {
+            animator.SetBool(WALK_PROPERTY, false);
+            return;
+        }
         var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
         var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentVelocity, smoothTime);
-        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+        // transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
         characterController.Move(direction *speed * Time.deltaTime);
+        animator.SetBool(WALK_PROPERTY, true);
     }
 
     public void onJump(InputAction.CallbackContext context){
         if (!context.started){
-            Debug.Log("Not jumping");
             return;
         }
         if (!IsGrounded()){

e244451445c19cab081d357076555b74bd17b38c
otteropossum <soccerpenguindude@gmail.com>
2023-09-25 17:09:59 -0500

    Worldspacetiling shadows and Floor block materials
    Added shadow support for materials made with worldspacetiling shader (receive shadows only)
    You can now change the material of a floor block in world gen.

--- Assets/Scripts/WorldGen.cs
+++ Assets/Scripts/WorldGen.cs
@@ -1,10 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class WorldGen : MonoBehaviour
 {
     public Transform levelPrefab;
+    public Material[] floorMaterials;
+
+
 
     [Range(0, 5)]
     public int numUndergroundLevels;
@@ -29,27 +31,128 @@ public class WorldGen : MonoBehaviour
         //Generate levels
         for (int x = 0; x < levels.Length; x++)
         {
-            Level currentLevel = levels[x];
-
-            for (int y = 0; y < levels[x].floorBlocks.Length; y++)
-            {
-                FloorBlock currentFloor = currentLevel.floorBlocks[y];
-
-
-                //how to add floors seemlessly together?
-                Transform floorBlock = Instantiate(levelPrefab, worldHolder);
-                floorBlock.localPosition = new Vector3(currentLevel.levelPosition.x + currentFloor.floorPosition.x, (x - numUndergroundLevels) * 5, currentLevel.levelPosition.y + currentLevel.floorBlocks[y].floorPosition.y);
-                floorBlock.localScale = currentFloor.floorScale;
-            }
-
+            GenerateLevel(x);
         }
 
     }
 
-    //Generate a level (will be moved here eventually!!! >.<
-    public void GenerateLevel()
-    {
 
+    //ok there seriously has to be a better way of doing this...
+    public Material GetFloorMaterial(FloorBlock.Color color) {
+
+        //ensures that there are more materials than floorblock colors as otherwise there can be a null reference exception
+        if(Enum.GetNames(typeof(FloorBlock.Color)).Length <= floorMaterials.Length){
+            if (color == FloorBlock.Color.BLUE) {
+                return floorMaterials[0];
+            }
+            if (color == FloorBlock.Color.BROWN)
+            {
+                return floorMaterials[1];
+            }
+            if (color == FloorBlock.Color.GRAY)
+            {
+                return floorMaterials[2];
+            }
+            if (color == FloorBlock.Color.GREEN)
+            {
+                return floorMaterials[3];
+            }
+            if (color == FloorBlock.Color.PINK)
+            {
+                return floorMaterials[4];
+            }
+            if (color == FloorBlock.Color.PURPLE)
+            {
+                return floorMaterials[5];
+            }
+            if (color == FloorBlock.Color.YELLOW)
+            {
+                return floorMaterials[6];
+            }
+        }
+        return null;
+    }
+
+
+    //Generate a level (will be moved here eventually!!! >.<
+    private void GenerateLevel(int index)
+    {
+        Level currentLevel = levels[index];
+        currentLevel.index = index;
+        for (int y = 0; y < levels[index].floorBlocks.Length; y++)
+        {
+            GenerateFloor(y, currentLevel);
+        }
+    }
+
+    //Index = the floor's index within the current levels floorBlocks[]
+    private void GenerateFloor(int index, Level currentLevel)
+    {
+        FloorBlock currentFloor = currentLevel.floorBlocks[index];
+        //Floorblock is the actual transform we are modifying rn
+        Transform floorBlock = Instantiate(levelPrefab, transform.GetChild(0));
+
+        //Set the scale of the floor
+        if (currentFloor.floorScale.x >= 0 && currentFloor.floorScale.y >= 0 && currentFloor.floorScale.z >= 0)
+        {
+            floorBlock.localScale = currentFloor.floorScale;
+        }
+        else
+        {
+            //Currently this will be spammed... but that's ok!! it really gets the point across then.
+            Debug.Log("Floorblock scale must be greater than 0");
+            return;
+        }
+
+        //  Set the position of the floor
+        // A floor will be magnetic if magenticTo is 1. less than the length of total floors 2. greater than zero 3. not equal to the current floor index
+        // Otherwise the position can be set wherever
+
+        //TODO: make it so there can't be a loop of magneticism (that might be really hard....)
+        bool lessTotalFloors = currentFloor.magneticTo < levels[currentLevel.index].floorBlocks.Length;
+        bool gteZero = currentFloor.magneticTo >= 0;
+        bool notCurrentFloor = !(currentFloor.magneticTo == index);
+
+        if (lessTotalFloors && gteZero && notCurrentFloor)
+        {
+            
+            FloorBlock magneticFloor = currentLevel.floorBlocks[currentFloor.magneticTo];
+
+            float positionx = magneticFloor.floorPosition.x + magneticFloor.floorScale.x / 2 + currentFloor.floorScale.x / 2;
+            float positiony = (currentLevel.index - numUndergroundLevels) * 5;
+            float positionz = magneticFloor.floorPosition.y;
+
+            currentFloor.floorPosition = new Vector2(positionx, positionz);
+
+            floorBlock.localPosition = new Vector3(currentLevel.levelPosition.x + positionx, positiony, currentLevel.levelPosition.y + positionz);
+            
+
+        }
+        else
+        {
+            float positionx = currentFloor.floorPosition.x;
+            float positiony = (currentLevel.index - numUndergroundLevels) * 5;
+            float positionz = currentFloor.floorPosition.y;
+            floorBlock.localPosition = new Vector3(currentLevel.levelPosition.x + positionx, positiony, currentLevel.levelPosition.y + positionz);
+            currentFloor.truePosition = new Vector3(positionx, positiony, positionz);
+        }
+
+        //Set the material of the floor
+
+        //This is assuming the transform that takes the floor tiles is the first child of the floorblock prefab (WHICH IT SHOULD!!!!)
+        Transform floorTile = floorBlock.GetChild(0);
+        if (floorTile)
+        {
+            if (GetFloorMaterial(currentFloor.color))
+            {
+                floorTile.GetComponent<MeshRenderer>().material = GetFloorMaterial(currentFloor.color);
+            }
+        }
+    }
+
+    private void GenerateEnvironment()
+    {
+        //WOW very full and cool
     }
 }
 
@@ -58,14 +161,25 @@ public class WorldGen : MonoBehaviour
 public class Level
 {
     public Vector2 levelPosition;
-    public Vector3 levelScale;
     public FloorBlock[] floorBlocks;
-}
 
+    [HideInInspector]
+    public int index;
+}
 
 [System.Serializable]
 public class FloorBlock
 {
     public Vector2 floorPosition;
+
+    [HideInInspector]
+    public Vector2 truePosition;
+
+    //all of the scales components must be >= to 0
     public Vector3 floorScale;
+    public enum Color { BLUE, BROWN, GRAY, GREEN, PINK, PURPLE, YELLOW };
+    public Color color;
+
+    //it would be so cool for this to be a range between 0 and floorBlocks.length but idk how
+    public int magneticTo;
 }
--- Assets/Shaders/Worldspacetiling.shader
+++ Assets/Shaders/Worldspacetiling.shader
@@ -1,6 +1,3 @@
-// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
-// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
-
 Shader "Custom/Worldspacetiling"
 {
     Properties
@@ -9,7 +6,7 @@ Shader "Custom/Worldspacetiling"
     }
     SubShader
     {
-        Tags {"RenderType"="Opaque" }
+        Tags {"LightMode"="ForwardBase" }
         LOD 100
 
         Pass
@@ -18,39 +15,50 @@ Shader "Custom/Worldspacetiling"
             #pragma vertex vert
             #pragma fragment frag               
             #include "UnityCG.cginc"
+            #include "Lighting.cginc"
 
-            struct appdata
-            {
-                float4 vertex : POSITION;
-                float2 uv : TEXCOORD0;
-            };
+            #pragma multi_compile_fwdbase nolightmap nodirlightmap nodynlightmap novertexlight
+            #include "AutoLight.cginc"
 
             struct v2f
             {
                 float2 uv : TEXCOORD0;
-                float4 vertex : SV_POSITION;
+                float4 pos : SV_POSITION;
+                SHADOW_COORDS(1) // put shadows data into TEXCOORD1
+                fixed3 diff : COLOR0;
+                fixed3 ambient : COLOR1;
             };
 
             sampler2D _MainTex;
             float4 _MainTex_ST;
 
-            v2f vert (appdata v)
+            v2f vert (appdata_base v)
             {
                 v2f o;
-                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.pos = UnityObjectToClipPos(v.vertex);
 
                 // Gets the xy position of the vertex in worldspace.
                 float2 worldXZ = mul(unity_ObjectToWorld, v.vertex).xz;
-                
                 // Use the worldspace coords instead of the mesh's UVs.
                 o.uv = TRANSFORM_TEX(worldXZ, _MainTex);
 
+                half3 worldNormal = UnityObjectToWorldNormal(v.normal);
+                half nl = max(0, dot(worldNormal, _WorldSpaceLightPos0.xyz));
+                o.diff = nl * _LightColor0.rgb;
+                o.ambient = ShadeSH9(half4(worldNormal,1));
+                // compute shadows data
+                TRANSFER_SHADOW(o)
                 return o;
             }
 
             fixed4 frag (v2f i) : SV_Target
             {                   
                 fixed4 col = tex2D(_MainTex, i.uv);
+                // compute shadow attenuation (1.0 = fully lit, 0.0 = fully shadowed)
+                fixed shadow = SHADOW_ATTENUATION(i);
+                // darken light's illumination with shadow, keep ambient intact
+                fixed3 lighting = i.diff * shadow + i.ambient;
+                col.rgb *= lighting;
                 return col;
             }
             ENDCG

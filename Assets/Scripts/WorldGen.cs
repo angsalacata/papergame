ffb903a1161bcc8d535e0a13478fb04e98cb1a88
otteropossum <soccerpenguindude@gmail.com>
2023-10-02 16:42:06 -0500

    Added magneticism of floor blocks within worldgen
    Floor blocks can stick to another floor block depending on how close their edges are.

--- Assets/Scripts/WorldGen.cs
+++ Assets/Scripts/WorldGen.cs
@@ -1,15 +1,16 @@
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class WorldGen : MonoBehaviour
 {
     public Transform levelPrefab;
     public Material[] floorMaterials;
 
-
-
     [Range(0, 5)]
     public int numUndergroundLevels;
+    //how close a floor block needs to be to its magnetic floor block before sticking. Must be greater than 0.
+    public float magneticTolerance;
     public Level[] levels;
 
     void Start()
@@ -27,7 +28,6 @@ public class WorldGen : MonoBehaviour
 
         Transform worldHolder = new GameObject(holderName).transform;
         worldHolder.parent = transform;
-
         //Generate levels
         for (int x = 0; x < levels.Length; x++)
         {
@@ -104,7 +104,8 @@ public class WorldGen : MonoBehaviour
             return;
         }
 
-        //  Set the position of the floor
+        // Set the position of the floor
+        
         // A floor will be magnetic if magenticTo is 1. less than the length of total floors 2. greater than zero 3. not equal to the current floor index
         // Otherwise the position can be set wherever
 
@@ -113,20 +114,81 @@ public class WorldGen : MonoBehaviour
         bool gteZero = currentFloor.magneticTo >= 0;
         bool notCurrentFloor = !(currentFloor.magneticTo == index);
 
+        //I am 80% sure this is unnecessary but I don't want to break anything so im leaving it! .-.
+        currentFloor.truePosition = new Vector2(currentFloor.floorPosition.x,currentFloor.floorPosition.y);
         if (lessTotalFloors && gteZero && notCurrentFloor)
         {
             
             FloorBlock magneticFloor = currentLevel.floorBlocks[currentFloor.magneticTo];
 
-            float positionx = magneticFloor.floorPosition.x + magneticFloor.floorScale.x / 2 + currentFloor.floorScale.x / 2;
-            float positiony = (currentLevel.index - numUndergroundLevels) * 5;
-            float positionz = magneticFloor.floorPosition.y;
+            //Magneticism is potentially possible by checking if the area of two floor blocks are interecting and then have them not intersect (easier said than done >-<)
+            //This could also be done with colliders but that might be expensive...
+            //The important this is that this works even though this is a lot of if statements
 
-            currentFloor.floorPosition = new Vector2(positionx, positionz);
+            //difference between the x position of the NORTH edge of the current floor block and the x position of the SOUTH edge of the magnetic floor block.
+            float northEdgeDiff = (currentFloor.floorPosition.y + currentFloor.floorScale.z / 2) - (magneticFloor.truePosition.y - magneticFloor.floorScale.z / 2);
+            //difference between the x position of the SOUTH edge of the current floor block and the x position of the NORTH edge of the magnetic floor block.
+            float southEdgeDiff = (currentFloor.floorPosition.y - currentFloor.floorScale.z / 2) - (magneticFloor.truePosition.y + magneticFloor.floorScale.z / 2);
+            //difference between the x position of the WEST edge of the current floor block and the x position of the EAST edge of the magnetic floor block.
+            float westEdgeDiff = (currentFloor.floorPosition.x - currentFloor.floorScale.x / 2) - (magneticFloor.truePosition.x + magneticFloor.floorScale.x / 2);
+            //difference between the x position of the EAST edge of the current floor block and the x position of the WEST edge of the magnetic floor block.
+            float eastEdgeDiff = (currentFloor.floorPosition.x + currentFloor.floorScale.x / 2) - (magneticFloor.truePosition.x - magneticFloor.floorScale.x / 2);
 
-            floorBlock.localPosition = new Vector3(currentLevel.levelPosition.x + positionx, positiony, currentLevel.levelPosition.y + positionz);
-            
+            //This checks that the north and south edges of the current block fall inside the north and south edges of the magnetic block.
+            if (southEdgeDiff < 0 && northEdgeDiff > 0)
+            {
+                float positionx = currentFloor.floorPosition.x;
+                float positiony = (currentLevel.index - numUndergroundLevels) * 5;
+                float positionz = currentFloor.floorPosition.y;
 
+                //if the WEST edge of the current floor block is within magneticTolerance units of the EAST edge of the magnetic block
+                //the current floor block will stick to the magnetic floor block.
+                if (westEdgeDiff < magneticTolerance && westEdgeDiff > 0)
+                {
+                    positionx = magneticFloor.truePosition.x + magneticFloor.floorScale.x / 2 + currentFloor.floorScale.x / 2;
+                }
+                //if the EAST edge of the current floor block is within magneticTolerance units of the WEST edge of the magnetic block
+                //the current floor block will stick to the magnetic floor block.
+                else if (eastEdgeDiff > -magneticTolerance && eastEdgeDiff < 0)
+                {
+                    positionx = magneticFloor.truePosition.x - magneticFloor.floorScale.x / 2 - currentFloor.floorScale.x / 2;
+                }
+                floorBlock.localPosition = new Vector3(currentLevel.levelPosition.x + positionx, positiony, currentLevel.levelPosition.y + positionz);
+                currentFloor.truePosition = new Vector2(positionx, positionz);
+            }
+            //This checks that the east and west edges of the current block fall inside the east and west edges of the magnetic block
+            else if (westEdgeDiff < 0 && eastEdgeDiff > 0)
+            {
+                float positionx = currentFloor.floorPosition.x;
+                float positiony = (currentLevel.index - numUndergroundLevels) * 5;
+                float positionz = currentFloor.floorPosition.y;
+
+                //if the SOUTH edge of the current floor block is within magneticTolerance units of the NORTH edge of the magnetic block
+                //the current floor block will stick to the magnetic floor block.
+                if (southEdgeDiff < magneticTolerance && southEdgeDiff > 0)
+                {
+                    positionz = magneticFloor.truePosition.y + magneticFloor.floorScale.z / 2 + currentFloor.floorScale.z / 2;
+                }
+                //if the NORTH edge of the current floor block is within magneticTolerance units of the SOUTH edge of the magnetic block
+                //the current floor block will stick to the magnetic floor block.
+                else if (northEdgeDiff > -magneticTolerance && northEdgeDiff < 0)
+                {
+                    positionz = magneticFloor.truePosition.y - magneticFloor.floorScale.z / 2 - currentFloor.floorScale.z / 2;
+                }
+                floorBlock.localPosition = new Vector3(currentLevel.levelPosition.x + positionx, positiony, currentLevel.levelPosition.y + positionz);
+                currentFloor.truePosition = new Vector2(positionx, positionz);
+            }
+
+            //If none of these conditions are met, the current block will be placed at the floor position.
+            else
+            {
+                //same as below... (there's probably a way to combine them, but i never learned oops)
+                float positionx = currentFloor.floorPosition.x;
+                float positiony = (currentLevel.index - numUndergroundLevels) * 5;
+                float positionz = currentFloor.floorPosition.y;
+                floorBlock.localPosition = new Vector3(currentLevel.levelPosition.x + positionx, positiony, currentLevel.levelPosition.y + positionz);
+                currentFloor.truePosition = new Vector2(positionx, positionz);
+            }
         }
         else
         {
@@ -134,7 +196,7 @@ public class WorldGen : MonoBehaviour
             float positiony = (currentLevel.index - numUndergroundLevels) * 5;
             float positionz = currentFloor.floorPosition.y;
             floorBlock.localPosition = new Vector3(currentLevel.levelPosition.x + positionx, positiony, currentLevel.levelPosition.y + positionz);
-            currentFloor.truePosition = new Vector3(positionx, positiony, positionz);
+            currentFloor.truePosition = new Vector2(positionx, positionz);
         }
 
         //Set the material of the floor
